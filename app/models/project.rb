8dbf1acf5887f812d73d464e4134c66e3f3584b0
nishina <n.toshi.1.014@gmail.com>
2016-10-04 00:46:28 +0900

    complete tutorial of dotinstall

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
   # validates :title, presence: true
+  has_many :tasks
   validates :title,
             presence: { message: "入力してください"},
             length: { minimum: 3, message: "短すぎ"}
